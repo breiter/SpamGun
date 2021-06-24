772d68db1ddcf2fda7a460c9d94d88a2ffd40120
Brian Reiter <breiter@wolfereiter.com>
2021-06-24 18:34:15 +0200

    Tidy and tabs to spaces.

--- SpamGun/Main.cs
+++ SpamGun/Main.cs
@@ -7,69 +7,69 @@ using System.Threading.Tasks;
 
 namespace BrianReiter.Notification
 {
-	public class MainClass
-	{
-		public static void Main (string[] args)
-		{ 
-			string bodyPath = null, htmlBodyPath = null,subjectPath = null, dataPath = null;
-			string fromAddress = null, fromName = null;
-			bool help, whatif = false;
-			var p = new OptionSet()
-			{
-				{ "b|body=", "Required: Email {BODY} TEXT template path.", x => bodyPath = x },
-				{ "h|html=", "Optional: Email {BODY} HTML template path.", x => htmlBodyPath = x },
-				{ "s|subject=", "Required: Email {SUBJECT} TXT file template path.", x => subjectPath = x },
-				{ "d|data=", "Required: Email merge {DATA} TSV file path.", x => dataPath = x },
-				{ "f|from=", "Required: Email address to send from.", x => fromAddress = x },
-				{ "n|from-name=", "Optional: Friendly {FROM-NAME} of the sender.", x => fromName = x },
-				{ "whatif", "Process the data file and args but send no email.", x => whatif = (x != null) },
-				{ "?|help", "Show this message and exit.", x=> help = (x != null) }
-				
-			};
+    public class MainClass
+    {
+        public static void Main (string[] args)
+        { 
+            string bodyPath = null, htmlBodyPath = null,subjectPath = null, dataPath = null;
+            string fromAddress = null, fromName = null;
+            bool help, whatif = false;
+            var p = new OptionSet()
+            {
+                { "b|body=", "Required: Email {BODY} TEXT template path.", x => bodyPath = x },
+                { "h|html=", "Optional: Email {BODY} HTML template path.", x => htmlBodyPath = x },
+                { "s|subject=", "Required: Email {SUBJECT} TXT file template path.", x => subjectPath = x },
+                { "d|data=", "Required: Email merge {DATA} TSV file path.", x => dataPath = x },
+                { "f|from=", "Required: Email address to send from.", x => fromAddress = x },
+                { "n|from-name=", "Optional: Friendly {FROM-NAME} of the sender.", x => fromName = x },
+                { "whatif", "Process the data file and args but send no email.", x => whatif = (x != null) },
+                { "?|help", "Show this message and exit.", x=> help = (x != null) }
+                
+            };
 
-			p.Parse( args );
-			help = (bodyPath == null) || (subjectPath == null) || (dataPath == null);
-			if( help )
-			{
-				ShowHelp( p );
-				return;
-			}
+            p.Parse( args );
+            help = (bodyPath == null) || (subjectPath == null) || (dataPath == null);
+            if( help )
+            {
+                ShowHelp( p );
+                return;
+            }
 
-			var options = new OptionInfo
-			{
+            var options = new OptionInfo
+            {
                 BodyPath = bodyPath,
                 HtmlBodyPath = htmlBodyPath,
-				SubjectPath = subjectPath,
-				DataPath = dataPath,
-				FromEmail = fromAddress,
-				FromName = fromName
-			};
+                SubjectPath = subjectPath,
+                DataPath = dataPath,
+                FromEmail = fromAddress,
+                FromName = fromName
+            };
 
-			IConfigurationBuilder builder = new ConfigurationBuilder();
-			builder.AddJsonFile("appsettings.json")
-				.AddEnvironmentVariables()
-				.AddCommandLine(args);
-			IConfiguration configuration = builder.Build();
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+            IConfiguration configuration = builder.Build();
 
 
-			var notifier = new Notifier( configuration, options ) { WhatIf = whatif } ;
-			Task.Run(async () => await notifier.SendAsync(Console.Out));
-		}
+            var notifier = new Notifier( configuration, options ) { WhatIf = whatif } ;
+            Task.Run(async () => await notifier.SendAsync(Console.Out));
+        }
 
-		static void ShowHelp(OptionSet p)
-		{
-			Console.WriteLine( "Usage: dotnet SpamGun.dll OPTIONS+" );
-			Console.WriteLine( "SpamGun notifier. Merges subject text and body HTML with TSV data file." );
+        static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine( "Usage: dotnet SpamGun.dll OPTIONS+" );
+            Console.WriteLine( "SpamGun notifier. Merges subject text and body HTML with TSV data file." );
 
-			Console.WriteLine( "SpamGun.exe version {0}.",
-				Assembly.GetExecutingAssembly().GetName().Version );
-			Console.WriteLine();
-			Console.WriteLine( "Options:" );
-			p.WriteOptionDescriptions( Console.Out );
+            Console.WriteLine( "SpamGun.exe version {0}.",
+                Assembly.GetExecutingAssembly().GetName().Version );
+            Console.WriteLine();
+            Console.WriteLine( "Options:" );
+            p.WriteOptionDescriptions( Console.Out );
 
-			Console.WriteLine( "TSV format: \r\n\tEMAIL\r\n" );
-			Console.WriteLine ();
-			Console.WriteLine( "\n# character at the beginning of a line indicates a comment." );
-		}
-	}
+            Console.WriteLine( "TSV format: \r\n\tEMAIL\r\n" );
+            Console.WriteLine ();
+            Console.WriteLine( "\n# character at the beginning of a line indicates a comment." );
+        }
+    }
 }
--- SpamGun/Notifier.cs
+++ SpamGun/Notifier.cs
@@ -8,104 +8,101 @@ using System.Threading.Tasks;
 
 namespace BrianReiter.Notification
 {
-	public class Notifier
-	{
-		public FileInfo BodyFile { get; set; }
-        public FileInfo HtmlBodyFile { get; set; }
-		public FileInfo SubjectFile { get; set; }
-		public FileInfo DataFile { get; set; }
-		public string FromEmail { get; set; }
-		public string FromName { get; set; }
+    public class Notifier
+    {
+        readonly FileInfo BodyFile; 
+        readonly FileInfo HtmlBodyFile;
+        readonly FileInfo SubjectFile;
+        readonly FileInfo DataFile;
+        readonly string FromEmail;
+        readonly string FromName;
         public bool WhatIf { get; set; }
 
-		protected IConfiguration Configuration { get; set; }
-		protected SmtpClientService SmtpClient => new SmtpClientService(Configuration);
+        protected IConfiguration Configuration { get; set; }
+        protected SmtpClientService SmtpClient => new SmtpClientService(Configuration);
         public Notifier() { WhatIf = false; }
-		public Notifier( IConfiguration configuration, OptionInfo options ) : this()
-		{
-			Configuration = configuration;
+        public Notifier(IConfiguration configuration, OptionInfo options) : this()
+        {
+            Configuration = configuration;
             BodyFile = new FileInfo(options.BodyPath);
             HtmlBodyFile = null;
             if (!String.IsNullOrEmpty(options.HtmlBodyPath))
                 HtmlBodyFile = new FileInfo(options.HtmlBodyPath);
-			SubjectFile = new FileInfo( options.SubjectPath );
-			DataFile = new FileInfo( options.DataPath );
-			FromEmail = options.FromEmail;
+            SubjectFile = new FileInfo( options.SubjectPath );
+            DataFile = new FileInfo( options.DataPath );
+            FromEmail = options.FromEmail;
+            FromName = options.FromName;
+        }
 
-			FromName = options.FromName;
-		}
-
-		public async Task SendAsync(TextWriter output)
-		{
-			string bodyTemplate, htmlBodyTemplate = null, subjectTemplate;
-			using( var bodyReader = BodyFile.OpenText() )
-			{ bodyTemplate = bodyReader.ReadToEnd(); }
-            if (HtmlBodyFile != null)
-            {
-                using (var htmlBodyReader = HtmlBodyFile.OpenText())
-                { htmlBodyTemplate = htmlBodyReader.ReadToEnd(); }
-            }
+        public async Task SendAsync(TextWriter output)
+        {
+            string bodyTemplate, htmlBodyTemplate = null, subjectTemplate;
             using (var subjectReader = SubjectFile.OpenText())
             { subjectTemplate = subjectReader.ReadLine(); }
-			MailboxAddress fromAddress = new MailboxAddress(FromName, FromEmail);
+            using( var bodyReader = BodyFile.OpenText() )
+            { bodyTemplate = bodyReader.ReadToEnd(); }
+            if (HtmlBodyFile != null)
+            {
+                using var htmlBodyReader = HtmlBodyFile.OpenText();
+                htmlBodyTemplate = htmlBodyReader.ReadToEnd();
+            }
 
+            MailboxAddress fromAddress = new MailboxAddress(FromName, FromEmail);
 
-			using(var dataReader = DataFile.OpenText())
-			{
-				if( WhatIf )
-				{ output.WriteLine( "*** WhatIf mode. No messages will be sent. ***" ); }
+            using var dataReader = DataFile.OpenText();
+            if (WhatIf)
+            { output.WriteLine("*** WhatIf mode. No messages will be sent. ***"); }
 
-				string line, name;
-				long lineNumber = 0;
-				while( null != (line = dataReader.ReadLine()) )
-				{
-					lineNumber++;
-					line = line.Trim();
-                    name = "Applicant";
-					if( line == string.Empty || line.StartsWith( "#" ) ) { continue ; }
+            string line, name;
+            long lineNumber = 0;
+            while (null != (line = dataReader.ReadLine()))
+            {
+                lineNumber++;
+                line = line.Trim();
+                name = "Applicant";
+                if (line == string.Empty || line.StartsWith("#")) { continue; }
 
-					var message = new MimeMessage();
-                    if (line.Contains("\t")) 
+                var message = new MimeMessage();
+                if (line.Contains("\t"))
+                {
+                    var fields = line.Split('\t');
+                    line = fields[0];
+                    name = !String.IsNullOrEmpty(fields[1]) ? fields[1] : "Applicant";
+                }
+
+                message.From.Add(fromAddress);
+                message.Subject = subjectTemplate;
+                var builder = new BodyBuilder()
+                {
+                    TextBody = string.Format(bodyTemplate, name)
+                };
+
+                if (!String.IsNullOrEmpty(htmlBodyTemplate))
+                {
+                    builder.HtmlBody = string.Format(htmlBodyTemplate, name);
+                }
+
+                message.Body = builder.ToMessageBody();
+                try
+                {
+                    message.To.Add(new MailboxAddress(name: null, address: line));
+
+                    if (WhatIf)
                     {
-                        var fields = line.Split('\t');
-                        line = fields[0];
-                        name = !String.IsNullOrEmpty(fields[1]) ? fields[1] : "Applicant";
+                        output.WriteLine("Line {0}: Notification would be to \"{1}\".", lineNumber, line);
                     }
-
-                    message.From.Add(fromAddress);
-					message.Subject = subjectTemplate;
-					var builder = new BodyBuilder()
-					{
-						TextBody = string.Format(bodyTemplate, name)
-					};
-
-                    if (!String.IsNullOrEmpty(htmlBodyTemplate))
+                    else
                     {
-						builder.HtmlBody = string.Format(htmlBodyTemplate, name);
+                        await SmtpClient.SendMessageAsync(message);
+                        output.WriteLine("Line {0}: Notification sent to \"{1}\".", lineNumber, line);
                     }
-
-					message.Body = builder.ToMessageBody();
-					try
-					{  
-						message.To.Add(new MailboxAddress(name:null, address:line));
-
-						if( WhatIf )
-						{
-							output.WriteLine( "Line {0}: Notification would be to \"{1}\".", lineNumber, line);
-						}
-						else
-						{
-							await SmtpClient.SendMessageAsync(message);
-							output.WriteLine( "Line {0}: Notification sent to \"{1}\".", lineNumber, line);
-						}
-					}
-					catch( Exception ex )
-					{
-						output.WriteLine( "Line {0}: Error. {1}", lineNumber, ex.Message );
-					}
-				}
-			}
-		}
-	}
+                }
+                catch (Exception ex)
+                {
+                    output.WriteLine("Line {0}: Error. {1}", lineNumber, ex.Message);
+                }
+            }
+        }
+    }
 }
 
